d8a035ae33ba8c98e8e514317d8fad8ccea60415
Alex <17277800+alex-schaaf@users.noreply.github.com>
2020-11-04 15:29:34 +0100

    switch to gorilla mux for routing

--- working/main.go
+++ working/main.go
@@ -9,14 +9,26 @@ import (
 	"time"
 
 	"github.com/alex-schaaf/learning-go-microservices/working/handlers"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	ph := handlers.NewProducts(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
+
+	// sm.Handle("/products", ph)
 
 	s := &http.Server{
 		Addr:         ":9090",
